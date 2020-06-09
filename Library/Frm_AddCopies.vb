91f1d4f3a7eee338c9b4433973d55d072bef6560
kazigani007 <kazigani03@gmail.com>
2020-06-09 12:28:10 +0200

    Change add copies form

--- Library/Frm_AddCopies.vb
+++ Library/Frm_AddCopies.vb
@@ -1,25 +1,28 @@
 ﻿Imports System.Data.OleDb
 Public Class Frm_AddCopies
-    Private db As New OleDbConnection(Connect)
+    Private dbConnection As New OleDbConnection(Connect)
+
     'Adding Copies
     Private Sub addcopies()
         Try
-            Dim insrtCopies As New OleDbCommand("Insert Into BookCopies Values('" & Tb_AN.Text & "','" & Frm_MngBooks.Tb_BC.Text & "',YES)", db)
-            db.Open()
+            Dim insrtCopies As New OleDbCommand("Insert Into BookCopies Values('" & Tb_AN.Text & "','" & Frm_MngBooks.Tb_BC.Text & "',YES)", dbConnection)
+            dbConnection.Open()
             insrtCopies.ExecuteNonQuery()
-            db.Close()
+            dbConnection.Close()
             Frm_MngBooks.DgvBookCopies()
             MessageBox.Show("The Book Has Been Successfully Added", "Successfully Added ", MessageBoxButtons.OK, MessageBoxIcon.Information)
         Catch ex As OleDb.OleDbException
-            db.Close()
+            dbConnection.Close()
             MessageBox.Show("The Book is Already Exist!" & vbNewLine & "Please Check For Accession Number", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
         End Try
     End Sub
+
     'Adding Button
     Private Sub Bttn_AddCop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Bttn_AddCop.Click
         addcopies()
         NofBooksCopies += 1
     End Sub
+
     'Form Load
     Private Sub Frm_AddCopies_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Label1.Text = Frm_MngBooks.Dgv_Books.CurrentRow.Cells(1).Value
@@ -30,6 +33,4 @@ Public Class Frm_AddCopies
             addcopies()
         End If
     End Sub
-
-    
 End Class
