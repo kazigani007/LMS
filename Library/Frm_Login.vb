8f21b71a9402a6627d39410cdbf87a5b28939402
kazigani007 <kazigani03@gmail.com>
2020-06-09 13:34:02 +0200

    Change login form

--- Library/Frm_Login.vb
+++ Library/Frm_Login.vb
@@ -1,12 +1,15 @@
 ﻿Imports System.Data.OleDb
 Public Class Frm_Login
-    Private db As New OleDbConnection(Connect)
+    Private dbConnection As New OleDbConnection(Connect)
+
     'Login
     Private Sub Login()
-        Dim Dgv As New OleDbDataAdapter("Select * From User_s Where UserName = '" & Tb_UserName.Text & "' And Psswrd = '" & Tb_Pass.Text & "';", db)
-        Dim Dtset As New DataSet
-        Dgv.Fill(Dtset)
-        If Dtset.Tables(0).DefaultView.Count > 0 Then
+        Dim dataAdapter As New OleDbDataAdapter("Select * From User_s Where 
+            UserName = '" & Tb_UserName.Text & "' And Psswrd = '" & Tb_Pass.Text & "';", dbConnection)
+        Dim dataSet As New DataSet
+        dataAdapter.Fill(dataSet)
+
+        If dataSet.Tables(0).DefaultView.Count > 0 Then
             Me.Hide()
             Frm_Main.Show()
         ElseIf Tb_UserName.Text = "" Then
@@ -15,10 +18,12 @@ Public Class Frm_Login
             MessageBox.Show("Username And/Or Password Are Invalid", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
         End If
     End Sub
+
     'Login Button
     Private Sub Bttn_Login_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Bttn_Login.Click
         Login()
     End Sub
+
     'Enter keys
     Private Sub Tb_Pass_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Tb_Pass.KeyDown
         If e.KeyCode = Keys.Enter Then
